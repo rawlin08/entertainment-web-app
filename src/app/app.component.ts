664746476f8b83727de4e10df4e778fef0bbd95c
Alec Rawlins <alec12999@yahoo.com>
2023-12-12 23:36:42 -0500

    get entertainment data on load

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as data from 'src/assets/data.json';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'entertainment-web-app';
+export class AppComponent implements OnInit {
+  constructor() {}
+  ngOnInit(): void {
+    this.data = JSON.parse(localStorage.getItem('entertainment')!);
+    if (!this.data) {
+      let entertainment = data;
+      localStorage.setItem('entertainment', JSON.stringify(entertainment.entertainment));
+      this.data = entertainment.entertainment;
+    }
+  }
+
+  data:any;
 }
