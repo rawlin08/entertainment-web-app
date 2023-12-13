88b374ec5ba525a1e61f8d22aaf681d3f4168994
Alec Rawlins <alec12999@yahoo.com>
2023-12-12 22:45:20 -0500

    search display

--- src/app/components/home.component.ts
+++ src/app/components/home.component.ts
@@ -2,8 +2,19 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-home',
-  template: ``,
-  styles: [``]
+  template: `
+  <div class="searchContainer">
+    <svg class="searchicon"><use href="#icon-search"></use></svg>
+    <input type="search" name="search" id="search" placeholder="Search for movies or TV series">
+  </div>
+  `,
+  styles: [`
+  .searchicon {
+    width: 32px;
+    height: 32px;
+    color: var(--white);
+  }
+  `]
 })
 export class HomeComponent {
 
