599d05a32e02922ca5825cabb7c74f43bcc5ae64
Alec Rawlins <alec12999@yahoo.com>
2023-12-12 22:27:33 -0500

    routes for nav

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home.component';
+import { MoviesComponent } from './components/movies.component';
+import { TvShowsComponent } from './components/tv-shows.component';
+import { BookmarkedComponent } from './components/bookmarked.component';
 
 const routes: Routes = [
   { path: 'home', title: 'Entertainment Home', component: HomeComponent },
+  { path: 'movies', title: 'Entertainment Movies', component: MoviesComponent },
+  { path: 'tv-shows', title: 'Entertainment TV Shows', component: TvShowsComponent },
+  { path: 'bookmarks', title: 'Bookmarks', component: BookmarkedComponent },
   { path: '', redirectTo: '/home', pathMatch: 'full' }
 ];
 
