69577121cb97dc96d1d159a39918b9b173f2f1ec
Alec Rawlins <alec12999@yahoo.com>
2023-12-13 00:08:06 -0500

    layout for details

--- src/app/components/recommended.component.ts
+++ src/app/components/recommended.component.ts
@@ -9,12 +9,15 @@ import { AppComponent } from '../app.component';
     <div class="regularcard card" *ngFor="let card of app.data">
       <div class="image">
         <img [src]="card.thumbnail.regular.small" alt="">
-        <svg class="bookmarkicon"><use href="#icon-bookmark-empty"></use></svg>
+        <svg class="bookmarkicon"><use attr.href="{{ card.isBookmarked == true ? '#icon-bookmark-full' : '#icon-bookmark-empty' }}"></use></svg>
       </div>
       <div class="details">
         <p>{{ card.year }}</p>
         &centerdot;
-        <p>{{ card.category }}</p>
+        <div class="category">
+          <svg class="detailsicon"><use attr.href="{{ card.category == 'Movie' ? '#icon-nav-movies' : '#icon-nav-tv-shows' }}"></use></svg>
+          <p>{{ card.category }}</p>
+        </div>
         &centerdot;
         <p>{{ card.rating }}</p>
       </div>
