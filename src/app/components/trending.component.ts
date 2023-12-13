d378c4f8d982669e377b22eceb7817b1873d0c9c
Alec Rawlins <alec12999@yahoo.com>
2023-12-13 00:08:16 -0500

    display trending entertainment

--- src/app/components/trending.component.ts
+++ src/app/components/trending.component.ts
@@ -1,10 +1,32 @@
 import { Component } from '@angular/core';
+import { AppComponent } from '../app.component';
 
 @Component({
   selector: 'app-trending',
-  template: ``,
+  template: `
+  <h2>Trending</h2>
+  <div class="regularcards">
+    <div class="regularcard card" *ngFor="let card of app.trending">
+      <div class="image">
+        <img [src]="card.thumbnail.regular.small" alt="">
+        <svg class="bookmarkicon"><use attr.href="{{ card.isBookmarked == true ? '#icon-bookmark-full' : '#icon-bookmark-empty' }}"></use></svg>
+      </div>
+      <div class="details">
+        <p>{{ card.year }}</p>
+        &centerdot;
+        <div class="category">
+          <svg class="detailsicon"><use attr.href="{{ card.category == 'Movie' ? '#icon-nav-movies' : '#icon-nav-tv-shows' }}"></use></svg>
+          <p>{{ card.category }}</p>
+        </div>
+        &centerdot;
+        <p>{{ card.rating }}</p>
+      </div>
+      <h3>{{ card.title }}</h3>
+    </div>
+  </div>
+  `,
   styles: [``]
 })
 export class TrendingComponent {
-
+  constructor(public app: AppComponent) {}
 }
