29b04c57c0082ba8616c8e60c90b62383367879e
Alec Rawlins <alec12999@yahoo.com>
2023-12-13 21:29:54 -0500

    integrate the bookmarked components together

--- src/app/components/bookmarked.component.ts
+++ src/app/components/bookmarked.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AppComponent } from '../app.component';
 
 @Component({
   selector: 'app-bookmarked',
-  template: ``,
+  template: `
+  <div class="searchContainer">
+    <svg class="searchicon"><use href="#icon-search"></use></svg>
+    <input type="search" name="search" id="search" placeholder="Search for movies or TV series">
+  </div>
+  <app-bookmarked-movies></app-bookmarked-movies>
+  <app-bookmarked-tv></app-bookmarked-tv>
+  `,
   styles: [``]
 })
-export class BookmarkedComponent {
+export class BookmarkedComponent implements OnInit {
+  constructor(public app: AppComponent) {}
+  ngOnInit(): void {
+    let bookmarked = this.app.data.filter((element:any) => element.isBookmarked == true);
+    this.bookmarkedTV = bookmarked.filter((element:any) => element.category == 'TV Series');
+    this.bookmarkedMovies = bookmarked.filter((element:any) => element.category == 'Movie');
+  };
 
+  bookmarkedTV:any;
+  bookmarkedMovies:any;
 }
