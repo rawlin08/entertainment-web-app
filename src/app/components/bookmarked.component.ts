eb434e69979dcee998fadfb2c718b39355ed9655
Alec Rawlins <alec12999@yahoo.com>
2023-12-13 22:59:04 -0500

    search for bookmarked shows

--- src/app/components/bookmarked.component.ts
+++ src/app/components/bookmarked.component.ts
@@ -6,21 +6,54 @@ import { AppComponent } from '../app.component';
   template: `
   <div class="searchContainer">
     <svg class="searchicon"><use href="#icon-search"></use></svg>
-    <input type="search" name="search" id="search" placeholder="Search for movies or TV series">
+    <input #searchInput (input)="search(searchInput.value)" type="search" name="search" id="search" placeholder="Search for movies or TV series">
   </div>
-  <app-bookmarked-movies></app-bookmarked-movies>
-  <app-bookmarked-tv></app-bookmarked-tv>
+  <div *ngIf="searchInput.value != ''" class="search">
+    <h2>Found {{ searched.length }} results for '{{ searchInput.value }}'</h2>
+    <div class="regularcards">
+      <div class="regularcard card" *ngFor="let card of searched">
+        <div class="image">
+          <img [src]="card.thumbnail.regular.small" alt="">
+          <svg class="bookmarkicon"><use attr.href="{{ card.isBookmarked == true ? '#icon-bookmark-full' : '#icon-bookmark-empty' }}"></use></svg>
+        </div>
+        <div class="details">
+          <p>{{ card.year }}</p>
+          &centerdot;
+          <div class="category">
+            <svg class="detailsicon"><use attr.href="{{ card.category == 'Movie' ? '#icon-nav-movies' : '#icon-nav-tv-shows' }}"></use></svg>
+            <p>{{ card.category }}</p>
+          </div>
+          &centerdot;
+          <p>{{ card.rating }}</p>
+        </div>
+        <h3>{{ card.title }}</h3>
+      </div>
+    </div>
+  </div>
+  <app-bookmarked-movies *ngIf="searchInput.value == ''"></app-bookmarked-movies>
+  <app-bookmarked-tv *ngIf="searchInput.value == ''"></app-bookmarked-tv>
   `,
   styles: [``]
 })
 export class BookmarkedComponent implements OnInit {
   constructor(public app: AppComponent) {}
   ngOnInit(): void {
-    let bookmarked = this.app.data.filter((element:any) => element.isBookmarked == true);
-    this.bookmarkedTV = bookmarked.filter((element:any) => element.category == 'TV Series');
-    this.bookmarkedMovies = bookmarked.filter((element:any) => element.category == 'Movie');
+    this.bookmarked = this.app.data.filter((element:any) => element.isBookmarked == true);
+    this.bookmarkedTV = this.bookmarked.filter((element:any) => element.category == 'TV Series');
+    this.bookmarkedMovies = this.bookmarked.filter((element:any) => element.category == 'Movie');
   };
 
+  bookmarked:any;
   bookmarkedTV:any;
   bookmarkedMovies:any;
+  searched:any = [];
+  search(terms:any) {
+    terms = terms.toLowerCase();
+    if (terms == '') {
+      this.searched = [];
+    }
+    else {
+      this.searched = this.bookmarked.filter((element:any) => element.title.toLowerCase().includes(terms));
+    }
+  }
 }
