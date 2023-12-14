10a7731e379477200f482a4cb9c80e9164e6c3d8
Alec Rawlins <alec12999@yahoo.com>
2023-12-13 21:30:26 -0500

    display and functions for the home component movies

--- src/app/components/movies.component.ts
+++ src/app/components/movies.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AppComponent } from '../app.component';
 
 @Component({
   selector: 'app-movies',
-  template: ``,
+  template: `
+  <div class="searchContainer">
+    <svg class="searchicon"><use href="#icon-search"></use></svg>
+    <input type="search" name="search" id="search" placeholder="Search for movies or TV series">
+  </div>
+  <app-all-movies></app-all-movies>
+  `,
   styles: [``]
 })
-export class MoviesComponent {
-
+export class MoviesComponent implements OnInit {
+  constructor(public app: AppComponent) {}
+  ngOnInit(): void {
+    this.movies = this.app.data.filter((element:any) => element.category == 'Movie');
+  }
+  movies:any;
 }
